372b55f420353eca1f6f6ae47fb29890588f4e95
over40dev <code@over40.dev>
2020-06-22 22:11:22 -0230

    edu-06-draw-sprites

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-import {test} from './Game.js';
+const { init, GameLoop, Sprite } = kontra;
+const { canvas, context } = init();
 
-console.log(test);
+const sprite = Sprite({
+  x: 200,
+  y: 200,
+  color: 'blue',
+  width: 50,
+  height: 20,
+  dx: 2,
+});
+
+// create main game loop
+const loop = GameLoop({
+  // update the game state
+  update: () => { 
+    sprite.update();
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
+  },
+  // render the game state
+  render: () => { 
+    sprite.render();
+  },
+});
+
+loop.start();

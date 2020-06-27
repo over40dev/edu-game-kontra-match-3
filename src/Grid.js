d9e4a0330805bd74b658ea68ad4a874ed96863b8
over40dev <code@over40.dev>
2020-06-27 20:40:25 -0230

    edu-13-drawing-the-grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,3 +1,5 @@
+const { Sprite } = kontra;
+
 export default class Grid {
   constructor(config) {
     this.numberOfRows = config.numberOfRows;
@@ -6,7 +8,54 @@ export default class Grid {
     this.color = config.color;
     this.x = config.x;
     this.y = config.y;
-    this.height = config.height;
-    this.width = config.width;
+
+    this.backgroundSprite = null;
+    this.width = this.numberOfCols * this.cellSize;
+    this.height = this.numberOfRows * this.cellSize;
+
+    this.gridSprites = [];
+
+    this.init();
+  }
+
+  init() {
+    this.backgroundSprite = Sprite({
+      x: this.x,
+      y: this.y,
+      color: 'grey',
+      width: this.width,
+      height: this.height,
+    });
+
+    // vertical grid lines
+    for (let i = 0; i < this.width + this.cellSize; i += this.cellSize) {
+      const sprite = Sprite({
+        x: this.x + i,
+        y: this.y,
+        color: this.color,
+        width: 1,
+        height: this.height,
+      });
+      this.gridSprites.push(sprite);
+    }
+
+    // horizontal grid lines
+    for (let i = 0; i < this.height + this.cellSize; i += this.cellSize) {
+      const sprite = Sprite({
+        x: this.x,
+        y: this.y + i,
+        color: this.color,
+        width: this.width,
+        height: 1,
+      });
+      this.gridSprites.push(sprite);
+    }
+  }
+
+  render() {
+    if (this.backgroundSprite) {
+      this.backgroundSprite.render();
+    }
+    this.gridSprites.forEach(s => { s.render(); });
   }
 }
